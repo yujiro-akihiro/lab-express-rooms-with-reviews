e4343873ad9259c99737aa11763a6a30d4e9d423
Yujiro Akihiro <yujiro.akihiro@gmail.com>
2024-09-11 14:17:13 +0200

    #3 Iteration 1 config/passport.js

---
+++ rooms-app-self/config/passport.js
@@ -0,0 +1,23 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/User')
+const bcrypt = require('bcrypt');
+
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+}, (email, password, done) => {
+    User.findOne({ email }, (err, user) => {
+        if (err) return done(err);
+        if (!user) return done(null, false, { message: 'Incorrect email.' });
+
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+            if (err) return done(err);
+            if (isMatch) {
+                return done(null, user);
+            } else {
+                return done(null, false, { message: 'Incorrect password.' });
+            }
+        });
+    });
+}));
--- rooms-app-self/models/User.model.js
+++ rooms-app-self/models/User.model.js
@@ -1,29 +1,24 @@
-/// Import required modules from Mongoose and bcrypt
 const { Schema, model } = require("mongoose");
 const bcrypt = require('bcrypt');
 
-// Define the user schema with email, password, fullName, and optional social login fields
 const userSchema = new Schema(
   {
     email: {
       type: String,
-      required: true, // "require" should be "required"
+      required: true,
       unique: true
     },
     password: {
       type: String,
-      required: true // "require" should be "required"
+      required: true,
+      minlength: 8 // Minimum password length set to 8
     },
     fullName: {
       type: String,
-      required: true // "require" should be "required"
+      required: true
     },
-    slackID: {
-      type: String // Optional field for Slack login
-    },
-    googleID: {
-      type: String // Optional field for Google login
-    }
+    slackID: String, // Optional field for Slack login
+    googleID: String // Optional field for Google login
   },
   {
     timestamps: true // Automatically add createdAt and updatedAt fields
@@ -39,7 +34,10 @@ userSchema.pre('save', function(next) {
 
   // Hash the password with bcrypt and assign it to the user object
   bcrypt.hash(user.password, 10, (err, hash) => {
-    if (err) return next(err);
+    if (err) {
+      console.error('Error hashing password:', err);
+      return next(err);
+    }
 
     // Set the user's password to the hashed value
     user.password = hash;
@@ -49,8 +47,5 @@ userSchema.pre('save', function(next) {
   });
 });
 
-// Create and export the User model
 const User = model("User", userSchema);
 module.exports = User;
-
-
